8cdc0f66204316908b866d3f2363881f907de9a3
jennifer-fintlabs <jennifer@fintlabs.no>
2023-04-17 15:40:12 +0200

    fix name of getTree for unit tree

--- src/repositories/UnitRepository.tsx
+++ src/repositories/UnitRepository.tsx
@@ -1,33 +1,13 @@
 import axios from 'axios';
 import {IUnitTree} from "../context/unitContext/types";
 
-
-
-const getPage = () => {
-
-
-    // Create an instance of Axios for the first proxy server
-    // const instance1 = axios.create({
-    //     baseURL: 'http://localhost:8081'
-    // });
-    //
+const getTree = () => {
     let url = `/api/orgunits/`;
-
-
-    // const headers = {
-    //     "Content-Type": "text/json",
-    // };
-    // return axios.get<IUnitTree>(url, {
-    //     headers: headers
-    // });
-
     return axios.get<IUnitTree>(url);
-
 }
 
 const UnitRepository = {
-    getUnitTree: getPage,
-    //TODO fix naming here
+    getUnitTree: getTree,
 };
 
 export default UnitRepository;
