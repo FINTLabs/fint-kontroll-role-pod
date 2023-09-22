d3103123b1b97ecaa009b654b1c193fb3cd5444f
jennifer-fintlabs <jennifer@fintlabs.no>
2023-09-22 15:55:53 +0200

    fixes to the url 3

--- src/features/main/DataTable.tsx
+++ src/features/main/DataTable.tsx
@@ -76,7 +76,7 @@ export const DataTable: any = () => {
                                 <TableCell align="left">{role.roleType} </TableCell>
                                 <TableCell align="left">
                                     <IconButton aria-label="settings" id={`role-${role.id}`}
-                                                component={Link} to={`/info/${role.id}`}
+                                                component={Link} to={`/grupper/info/${role.id}`}
                                     >
                                         <SettingsRounded color={"primary"}/>
                                     </IconButton>
