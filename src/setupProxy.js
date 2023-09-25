a6a33d0e31a5abeaa217400c07c2fe7f7d94c8da
jennifer-fintlabs <jennifer@fintlabs.no>
2023-09-25 16:10:09 +0200

    one more try before i give up for today

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
     app.use(
         '/api/roles',
         createProxyMiddleware({
-            target: 'http://localhost:8090/beta/fintlabs-no',
-            // target: 'http://localhost:8090',
+            // target: 'http://localhost:8090/beta/fintlabs-no',
+            target: 'http://localhost:8090',
             changeOrigin: true,
         })
     );
@@ -13,8 +13,8 @@ module.exports = function(app) {
     app.use(
         '/api/orgunits',
         createProxyMiddleware({
-            target: 'http://localhost:8081/beta/fintlabs-no',
-            // target: 'http://localhost:8081',
+            // target: 'http://localhost:8081/beta/fintlabs-no',
+            target: 'http://localhost:8081',
             changeOrigin: true,
         })
     );
@@ -22,8 +22,8 @@ module.exports = function(app) {
     app.use(
         '/api/layout/configuration',
         createProxyMiddleware({
-            target: 'http://localhost:8080/beta/fintlabs-no',
-            // target: 'http://localhost:8080',
+        // target: 'http://localhost:8080/beta/fintlabs-no',
+            target: 'http://localhost:8080',
             changeOrigin: true,
         })
     );
